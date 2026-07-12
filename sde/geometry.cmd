96027e96a25d83c4d4f8cb019ee343f009eec711
Allen Joe A <allenjoe.a2024@vitstudent.ac.in>
2026-07-12 22:49:32 +0530

    Create  geometry.cmd

---
+++ sde/geometry.cmd
@@ -0,0 +1,19 @@
+Source Width            = 10 nm
+Source Height           = 20 nm
+
+Pocket Width            = 10 nm
+Pocket Thickness        = 5 nm
+
+Channel Width           = 10 nm
+Channel Height          = 20 nm
+
+Drain Width             = 30 nm
+Drain Height            = 25 nm
+
+Drain Extension Height  = 10 nm
+Drain Contact Height    = 15 nm
+
+HfO₂ Thickness          = 2 nm
+SiO₂ Thickness          = 2 nm
+
+Substrate Thickness     = 20 nm
